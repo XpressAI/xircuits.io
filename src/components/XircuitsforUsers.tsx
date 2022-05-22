4b11fbfb5d20b5093a97a67138feaa88d3d16e7c
MFA-X-AI <68586800+MFA-X-AI@users.noreply.github.com>
2022-05-23 00:29:14 +0800

    fixed padding

--- src/components/XircuitsforUsers.tsx
+++ src/components/XircuitsforUsers.tsx
@@ -11,27 +11,30 @@ function XircuitsforUsers() {
 
         <div className={clsx('col col--3 col--offset-2')}>
           <br></br>
-          <h1>Xircuits for Users</h1>
-          <p>You don't have to know how to code to use Xircuits!</p>
-          <ul>
-              <li><strong>Create workflows</strong> from prebuilt components</li>
-              <li><strong>Easily modify</strong> templates from our <strong>growing repository</strong> for your personal use case</li>
-              <li>Our <strong>component tooltips </strong>will always be there to help you.</li>
-          </ul>
-          <p>From image classification to anomaly detection, perform complex workflows at a click of a button!</p>
+          <div className={styles.contentPad}>
+            <h1>Xircuits for Users</h1>
+            <p>You don't have to know how to code to use Xircuits!</p>
+            <ul>
+                <li><strong>Create workflows</strong> from prebuilt components</li>
+                <li><strong>Easily modify</strong> templates from our <strong>growing repository</strong> for your personal use case</li>
+                <li>Our <strong>component tooltips </strong>will always be there to help you.</li>
+            </ul>
+            <p>From image classification to anomaly detection, perform complex workflows at a click of a button!</p>
 
-          <div className={styles.buttons}>
-            <Link
-              className="button button--outline button--primary button--lg"
-              to="/docs/category/user-guide">
-              Go to User Guide
-            </Link>
+            <div className={styles.buttons}>
+              <Link
+                className="button button--outline button--primary button--lg"
+                to="/docs/category/user-guide">
+                Go to User Guide
+              </Link>
+            </div>
           </div>
-
         </div>
 
         <div className={clsx('col col--5 col--offset-1')}>
-          <img src="/img/website/preview-machine-learning.gif"></img>
+          <div className={styles.contentPad}>
+            <img src="/img/website/preview-machine-learning.gif"></img>
+          </div>
         </div>
     </div>
     );
