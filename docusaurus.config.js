1f20759ac29cb76a72402994beadb807d3088428
MFA-X-AI <fahreza@xpress.ai>
2023-10-25 15:17:39 +0700

    add docusaurus vecto search, add configs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,11 +13,8 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/xpress-logo.ico',
-  organizationName: 'XpressAI', // Usually your GitHub org/user name.
-  projectName: 'Xircuits', // Usually your repo name.
-  themes: [
-    "docusaurus-vecto-search",
-  ],
+  organizationName: 'XpressAI',
+  projectName: 'Xircuits',
   presets: [
     [
       '@docusaurus/preset-classic',
@@ -37,7 +34,9 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      }
+      ),
+
     ],
   ],
 
@@ -152,8 +151,22 @@ const config = {
       colorMode: {
         defaultMode: 'light',
         disableSwitch: true
-      }
+      },
     }),
+
+    themes: [
+      [
+        "@xpressai/docusaurus-vecto-search",
+        /** type {import("@xpressai/docusaurus-vecto-search").PluginOptions} */
+        ({
+          docsRouteBasePath: '/',
+          vecto_public_token: process.env.vecto_public_token,
+          vector_space_id: Number(process.env.vector_space_id),
+          top_k: 20,
+          rankBy: "weightedAverage"
+        }),
+      ],
+    ],
 };
 
 module.exports = config;
